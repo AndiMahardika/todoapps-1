1d42409b8e8ac16d5aea2e47a0064001520074f0
AndiMahardika <andi.mahardika.mulya@gmail.com>
2023-09-11 18:28:08 -0700

    menambahkan fungsi generate id & generateTodoObject

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
+const todos = [];
+const RENDER_EVENT = `render_todo`;
+
 document.addEventListener(`DOMContentLoaded`,function(){
-    const submitForm = document.querySelector(`#form`);
+    const submitForm = document.getElementById(`form`);
 
     submitForm.addEventListener(`submit`,function(event){
         event.preventDefault();
@@ -8,32 +11,48 @@ document.addEventListener(`DOMContentLoaded`,function(){
 })
 
 function addTodo(){
-    const textTodo = document.querySelector(`#title`);
-    const timeStamp = document.querySelector(`#date`);
+    const textTodo = document.getElementById(`title`).value;
+    const timeStamp = document.getElementById(`date`).value;
 
     const generateID = generateId();
     const todoOject = generateTodoOject(generateID, textTodo, timeStamp, false);
-    todos.pusj(todoOject);
+    todos.push(todoOject);
 
     document.dispatchEvent(new Event(RENDER_EVENT));
 }
 
-
-function makeTodo(todoOject){
-    const textTitle = document.createElement(`h2`);
-    textTitle.innerText = todoOject.task;
-
-    const textTimeStamp = document.createElement(`p`);
-    textTimeStamp.innerText = todoOject.timeStamp;
-
-    const textContainer = document.createElement(`div`);
-    textContainer.classList.add(`inner`);
-    textContainer.appendChild(textTitle, textTimeStamp);
-
-    const container = document.createElement(`div`);
-    container.classList.add(`item`, `shadow`);
-    container.appendChild(textContainer);
-    container.setAttribute(`id`, `todo-${todoOject.id}`)
-
-    return container;
+function generateId(){
+    return +new Date();
 }
+
+function generateTodoOject(id, task, timestamp, isCompleted){
+    return {
+        id,
+        task,
+        timestamp,
+        isCompleted
+    }
+}
+
+document.addEventListener(RENDER_EVENT, function () {
+    console.log(todos);
+  });
+
+// function makeTodo(todoOject){
+//     const textTitle = document.createElement(`h2`);
+//     textTitle.innerText = todoOject.task;
+
+//     const textTimeStamp = document.createElement(`p`);
+//     textTimeStamp.innerText = todoOject.timeStamp;
+
+//     const textContainer = document.createElement(`div`);
+//     textContainer.classList.add(`inner`);
+//     textContainer.appendChild(textTitle, textTimeStamp);
+
+//     const container = document.createElement(`div`);
+//     container.classList.add(`item`, `shadow`);
+//     container.appendChild(textContainer);
+//     container.setAttribute(`id`, `todo-${todoOject.id}`)
+
+//     return container;
+// }
